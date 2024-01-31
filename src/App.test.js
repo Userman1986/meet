9c8822a264510110955ed167873f986367f9b063
Userman1986 <e.samonin1986@gmail.com>
2024-01-31 19:27:34 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// src/__tests__/App.test.js
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('<App /> component', () => {
+  test('renders list of events', () => {
+    const AppDOM = render(<App />).container.firstChild;
+    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+  });
+
 });
