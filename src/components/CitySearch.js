61edb5865314d3dffbd34fdcef82ba6df746a842
Userman1986 <e.samonin1986@gmail.com>
2024-02-01 10:04:34 +0100

    qwr

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,20 +1,38 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
+
   const handleInputChanged = (event) => {
     const value = event.target.value;
-    const filteredLocations = allLocations
-      ? allLocations.filter((location) => {
-          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-        })
-      : [];
-
+    const filteredLocations = allLocations ? allLocations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    }) : [];
     setQuery(value);
     setSuggestions(filteredLocations);
+
+    // Alerts
+    let infoText;
+    if (filteredLocations.length === 0) {
+      infoText = "We can not find the city you are looking for. Please try another city"
+    } else {
+      infoText = ""
+    }
+    setInfoAlert(infoText);
+  };
+  
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false); // to hide the list
+    setCurrentCity(value);
+    setInfoAlert("")
   };
 
   return (
@@ -27,20 +45,19 @@ const CitySearch = ({ allLocations }) => {
         onFocus={() => setShowSuggestions(true)}
         onChange={handleInputChanged}
       />
-      {showSuggestions ? (
+      {showSuggestions ?
         <ul className="suggestions">
-          {suggestions.map((suggestion) => (
-            <li key={suggestion}>{suggestion}</li>
-          ))}
-          {suggestions.length === 0 && (
-            <li>
-              <b>No matching cities</b>
-            </li>
-          )}
+          {suggestions.map((suggestions) => {
+            return <li onClick={handleItemClicked} key={suggestions}>{suggestions}</li>
+          })}
+          <li key='See all cities'onClick={handleItemClicked}>
+            <b>See all cities</b>
+          </li>
         </ul>
-      ) : null}
+        : null
+      }
     </div>
-  );
-};
+  )
+}
 
 export default CitySearch;
