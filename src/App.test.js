d1966aeb0aa2d0356c454cfd6d2ce8749ba89e4b
Userman1986 <e.samonin1986@gmail.com>
2024-02-01 08:21:09 +0100

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 // src/__tests__/App.test.js
 
 import { render } from '@testing-library/react';
-import App from '../App';
+import App from './App';
 
 describe('<App /> component', () => {
   test('renders list of events', () => {
