dfa79d86ec7726ade071d592501d7b276984b504
Userman1986 <e.samonin1986@gmail.com>
2024-02-05 17:35:55 +0100

    fqef

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@
 import { useState, useEffect } from "react";
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 import { extractLocations, getEvents } from './api';
 
 const App = () => {
 
-const[allLocations, setAllLocations] = userState([]);
+const[allLocations, setAllLocations] = useState([]);
 const getAllLocations = async () => {
 const allEvents = await gerEvents ();
 const locations = extractLocations (allEvents[0].items);
+const [events, setEvents] = useState([]);
+const [currentNOE, setCurrentNOE] = useState(32);
+
+const fetchData = async () => {
+  const allEvents = await getEvents();
+  setEvents(allEvents.slice(0, currentNOE));
+}
 
 return locations;
 }
+
+useEffect(() => {
+  fetchData();
+}, []);
 
 
 useEffect (() => {
@@ -28,12 +40,13 @@ getAllLocations().then((locations) =>{
 
 
 
- return (
-   <div className="App">
-     <CitySearch allLocations={allLocations}/>
-     <EventList />
-   </div>
- );
+return (
+  <div className="App">
+    <CitySearch />
+    <NumberOfEvents />
+    <EventList events={events} />
+  </div>
+);
 }
 
 export default App;
