a2cd9e342d0ed3f6bc2a2e92227bbbff3662a510
Userman1986 <e.samonin1986@gmail.com>
2024-02-05 14:34:44 +0100

    4.4 changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 // src/App.js
 
+import { useState, useEffect } from "react";
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import './App.css';
-import { extractLocations } from './api';
+import { extractLocations, getEvents } from './api';
 
 const App = () => {
-  const events = getEvents();
-  const allLocations = extractLocations (events);
+
+const[allLocations, setAllLocations] = userState([]);
+const getAllLocations = async () => {
+const allEvents = await gerEvents ();
+const locations = extractLocations (allEvents[0].items);
+
+return locations;
+}
+
+
+useEffect (() => {
+getAllLocations().then((locations) =>{
+  setAllLocations(locations)
+});
+
+
+
+}, [] );
+
+
+
  return (
    <div className="App">
      <CitySearch allLocations={allLocations}/>
