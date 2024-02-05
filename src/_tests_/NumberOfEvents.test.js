87b46c27481747f4d7d0601e5a083d27e64a1040
Userman1986 <e.samonin1986@gmail.com>
2024-02-05 18:29:58 +0100

    fwq

---
+++ src/_tests_/NumberOfEvents.test.js
@@ -0,0 +1,47 @@
+import { render, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NumberOfEvents from '../components/NumberOfEvents';
+import App from '../App';
+
+describe('<NumberOfEvents /> component', () => {
+    let NumberOfEventsComponent;
+    beforeEach(() => {
+        NumberOfEventsComponent = render(<NumberOfEvents setCurrentNOE={() => {}} setErrorAlert={() => {}} />); //no setNumb ...?
+    });
+
+    test('renders number of events text input', () => {
+        const numberTextBox = NumberOfEventsComponent.queryByRole('spinbutton');
+        expect(numberTextBox).toBeInTheDocument();
+        expect(numberTextBox).toHaveClass('number-of-events-input');
+    });
+
+    test('default value of the input field is 32', () => {
+        const numberTextBox = NumberOfEventsComponent.queryByRole('spinbutton');
+        expect(numberTextBox).toHaveValue(32);
+    });
+    
+    test('value changes accordingly when user types', async() => {
+        const user = userEvent.setup();
+        const numberInput = NumberOfEventsComponent.queryByRole('spinbutton');
+        await user.type(numberInput, '{backspace}{backspace}10');
+        expect(numberInput).toHaveValue(10);
+    });
+})
+
+//  integration testing
+describe('<NumberOfEvents /> integration', () => {
+    test('user can change the number of events displayed',  async () => {
+        const user = userEvent.setup();
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+
+        const NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
+        const numberOfEventsInput = within(NumberOfEventsDOM).queryByRole('spinbutton');
+        await user.type(numberOfEventsInput, '{backspace}{backspace}10');
+
+        const EventListDOM = AppDOM.querySelector('#event-list');
+
+        const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
+        expect(allRenderedEventItems.length).toEqual(10);
+    });
+});
