3d4e2a081e24502542a3d6843a916a1c033ba634
Userman1986 <e.samonin1986@gmail.com>
2024-02-09 10:18:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+<<<<<<< HEAD
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 
 
+=======
+import { getEvents } from './api';
+>>>>>>> parent of 289f42b (revert)
 import './App.css';
 
 const App = () => {
@@ -18,7 +22,30 @@ const App = () => {
 
   useEffect(() => {
     fetchData();
+<<<<<<< HEAD
   }, [currentCity, currentNOE]);
+=======
+  }, [currentCity, currentNOE]); 
+
+  const fetchData = async () => {
+    try {
+      const allEvents = await getEvents();
+      const extractedEvents = allEvents[0].items || []; 
+      setEvents(extractedEvents.slice(0, currentNOE));
+      setAllLocations(extractLocations(filteredEvents)); 
+      setErrorAlert(""); 
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setErrorAlert("Error fetching data. Please try again."); 
+    }
+  };
+
+  const extractLocations = (events) => {
+    const extractedLocations = events.map((event) => event.location);
+    const locations = [...new Set(extractedLocations)];
+    return locations;
+  };
+>>>>>>> parent of 289f42b (revert)
 
   const fetchData = async () => {
     const allEvents = await getEvents();
